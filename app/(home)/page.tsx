728621dc6b64e3e62174a2802313b4f7d6e5916d
kefflen <kefflenjava@hotmail.com>
2024-01-30 11:10:08 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,15 @@
+/* eslint-disable quotes */
+import { format } from 'date-fns'
 import Header from '../_components/header'
 
 const Home = () => {
 	return (
 		<div className=''>
 			<Header />
+			<div className="px-5 pt-5">
+				<h2>Ola, Miguel</h2>
+				<p>{format(new Date(), `EEEE, d 'de ' MMMM`)}</p>
+			</div>
 		</div>)
 }
  
