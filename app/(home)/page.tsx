6284979bcca1e32b86fd9eded4c4acd242c5422b
kefflen <kefflenjava@hotmail.com>
2024-01-30 13:45:08 -0300

    feat: add search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable quotes */
 import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import Header from '../_components/header'
+import Search from './_components/search'
 
 const Home = () => {
 	return (
@@ -8,7 +10,13 @@ const Home = () => {
 			<Header />
 			<div className="px-5 pt-5">
 				<h2>Ola, Miguel</h2>
-				<p>{format(new Date(), `EEEE, d 'de ' MMMM`)}</p>
+				<p className='capitalize text-sm'>{format(new Date(), `EEEE, d 'de ' MMMM`, {
+					locale: ptBR
+				})}</p>
+
+				<div className="px-5 mt-6">
+					<Search />
+				</div>
 			</div>
 		</div>)
 }
