f833bce288e1dd9c2a1be2c6fff60a005faebe96
kefflen <kefflenjava@hotmail.com>
2024-02-28 09:38:18 -0300

    feat: use booking props to render

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -1,8 +1,19 @@
+import { Prisma } from '@prisma/client'
+import { format } from 'date-fns'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { Badge } from './ui/badge'
 import { Card, CardContent } from './ui/card'
 
-const BookingItem = () => {
+interface BookingItemProps {
+  booking: Prisma.BookingGetPayload<{
+    include: {
+      service: true
+      barbershop: true
+    }
+  }>
+}
+
+const BookingItem = ({ booking }: BookingItemProps) => {
   return (
     <Card className="">
       <CardContent className="p-0 px-5 flex justify-between">
@@ -10,23 +21,19 @@ const BookingItem = () => {
           <Badge className="bg-[#221c3d] text-primary hover:bg-[#221c3d] w-fit">
             Confirmado
           </Badge>
-          <h2 className="font-bold">Corte de cabelo</h2>
+          <h2 className="font-bold">{booking.service.name}</h2>
           <div className="flex items-center gap-2">
             <Avatar className="h-6 w-6">
-              <AvatarImage
-                src={
-                  'https://utfs.io/f/8a457cda-f768-411d-a737-cdb23ca6b9b5-b3pegf.png'
-                }
-              />
+              <AvatarImage src={booking.barbershop.imageUrl} />
               <AvatarFallback>A</AvatarFallback>
             </Avatar>
-            <h3 className="text-sm">Vintage barber</h3>
+            <h3 className="text-sm">{booking.barbershop.name}</h3>
           </div>
         </div>
         <div className="flex flex-col justify-center px-3 items-center border-l-2 border-solid border-secondary">
-          <p className="text-sm">Fervereiro</p>
-          <p className="text-2xl">06</p>
-          <p className="text-sm">14:00</p>
+          <p className="text-sm">{format(booking.date, 'MMMM')}</p>
+          <p className="text-2xl">{format(booking.date, 'dd')}</p>
+          <p className="text-sm">{format(booking.date, 'hh:mm')}</p>
         </div>
       </CardContent>
     </Card>
