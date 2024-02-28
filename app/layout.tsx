fe12470c775bccf7a9b32d6134935409fe176ae2
kefflen <kefflenjava@hotmail.com>
2024-02-28 11:23:58 -0300

    feat: ensure footer in the end of page

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} dark`}>
         <AuthProvider>
-          {children}
+          <div className="flex-1">
+            {children}
+          </div>
           <Footer />
         </AuthProvider>
       </body>
